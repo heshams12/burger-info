5f3296503cca8641289fc4b2ba12cd583096ba99
Unknown <Heshams12@hotmail.com>
2018-03-26 13:06:51 -0700

    updated HW assignment

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+if (process.env.NODE_ENV !== 'production') 
+{
+  require('dotenv').load();
+}
+
+var connection = mysql.createConnection({
+  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_SCHEMA
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Meeses');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bobbi');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sylvester', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Marilyn Manson', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Joe Biden', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Owens');
